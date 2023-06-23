25ad8ae5c4b4d8facfca1d3ae93b4f9209f8735f
han1022 <shifty393@gmail.com>
2023-06-23 19:08:22 +0900

    recyclerView item 클릭 이벤트 구현중

--- app/src/main/java/com/example/voyage/MainRvAdapter.kt
+++ app/src/main/java/com/example/voyage/MainRvAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.voyage
 
 import android.content.Context
+import android.system.Os.bind
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -21,6 +22,18 @@ class MainRvAdapter (var add_schedule: ArrayList<AddSchedule>) : RecyclerView.Ad
         holder.content_tv.text = add_schedule[position].content
         holder.memo_tv.text = add_schedule[position].memo
         holder.endAt_tv.text = add_schedule[position].endTime
+
+        val itemList = add_schedule[position]
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, position)
+        }
+        holder.apply {
+            //CustomViewHolder(itemList)  //괄호 안에 나는 view가 들어가야하는데...
+            //bind는 괄호 안에 AddSchedule이 들어가는거임
+            //이거 진자 방법 없으면 bind 써서 싹 고쳐야함
+        }
+
     }
 
     override fun getItemCount(): Int {
@@ -32,6 +45,16 @@ class MainRvAdapter (var add_schedule: ArrayList<AddSchedule>) : RecyclerView.Ad
         var content_tv = itemView.findViewById<TextView>(R.id.tv_content)
         var memo_tv = itemView.findViewById<TextView>(R.id.tv_memo)
         var endAt_tv = itemView.findViewById<TextView>(R.id.tv_endAt)
+    }
+
+    //ClickListener
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
+    private lateinit var itemClickListener : OnItemClickListener
+
+    fun setItemClickListener(itemClickListener: OnItemClickListener) {
+        this.itemClickListener = itemClickListener
     }
 
 }
