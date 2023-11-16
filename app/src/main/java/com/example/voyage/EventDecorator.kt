f979f4b3847c0e2abcfcc3d00aa0b2723e7cbfa6
han1022 <shifty393@gmail.com>
2023-11-16 16:49:47 +0900

    dot 수정중

--- app/src/main/java/com/example/voyage/EventDecorator.kt
+++ app/src/main/java/com/example/voyage/EventDecorator.kt
@@ -1,11 +1,15 @@
 package com.example.voyage
 
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.text.style.LineBackgroundSpan
 import android.util.Log
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.spans.DotSpan
+import com.prolificinteractive.materialcalendarview.spans.DotSpan.DEFAULT_RADIUS
 
 class EventDecorator(dates: Collection<CalendarDay>) : DayViewDecorator {
     var dates: HashSet<CalendarDay> = HashSet(dates)
@@ -13,52 +17,66 @@ class EventDecorator(dates: Collection<CalendarDay>) : DayViewDecorator {
     override fun shouldDecorate(day: CalendarDay?): Boolean {
         return dates.contains(day)
     }
+
     override fun decorate(view: DayViewFacade?) {
-//        view?.addSpan(DotSpan(7F, Color.parseColor("#303f9f")))
-        if (App.prefs.getString("color", "") == "RED")
-            redDecorate(view)
-        if (App.prefs.getString("color", "") == "ORANGE")
-            orangeDecorate(view)
-        if (App.prefs.getString("color", "") == "YELLOW")
-            yellowDecorate(view)
-        if (App.prefs.getString("color", "") == "GREEN")
-            greenDecorate(view)
-        if (App.prefs.getString("color", "") == "DEEP GREEN")
-            deepGreenDecorate(view)
-        if (App.prefs.getString("color", "") == "BLUE")
-            blueDecorate(view)
-        if (App.prefs.getString("color", "") == "DEEP BLUE")
-            deepBlueDecorate(view)
-        if (App.prefs.getString("color", "") == "BROWN")
-            brownDecorate(view)
-        if (App.prefs.getString("color", "") == "GRAY")
-            grayDecorate(view)
+        view?.addSpan(CustomMultipleDotSpan(7f, Color.parseColor("303f9f")))
     }
-    private fun redDecorate(view: DayViewFacade?) {
+}
+
+class RedDecorate(dates: Collection<CalendarDay>) : DayViewDecorator {
+    var dates: HashSet<CalendarDay> = HashSet(dates)
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        return dates.contains(day)
+    }
+    override fun decorate(view: DayViewFacade?) {
         view?.addSpan(DotSpan(7F, Color.parseColor("#d32f2f")))
+        Log.d("DecoDates", "$dates")
     }
-    private fun orangeDecorate(view: DayViewFacade?) {
-        view?.addSpan(DotSpan(7F, Color.parseColor("#f57c00")))
+}
+
+class DeepBlueDecorate(dates: Collection<CalendarDay>) : DayViewDecorator {
+    var dates: HashSet<CalendarDay> = HashSet(dates)
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        return dates.contains(day)
     }
-    private fun yellowDecorate(view: DayViewFacade?) {
-        view?.addSpan(DotSpan(7F, Color.parseColor("#dfbc02")))
-    }
-    private fun greenDecorate(view: DayViewFacade?) {
-        view?.addSpan(DotSpan(7F, Color.parseColor("#689f38")))
-    }
-    private fun deepGreenDecorate(view: DayViewFacade?) {
-        view?.addSpan(DotSpan(7F, Color.parseColor("#00796b")))
-    }
-    private fun blueDecorate(view: DayViewFacade?) {
-        view?.addSpan(DotSpan(7F, Color.parseColor("#0288d1")))
-    }
-    private fun deepBlueDecorate(view: DayViewFacade?) {
+
+    override fun decorate(view: DayViewFacade?) {
         view?.addSpan(DotSpan(7F, Color.parseColor("#303f9f")))
     }
-    private fun brownDecorate(view: DayViewFacade?) {
-        view?.addSpan(DotSpan(7F, Color.parseColor("#5d4037")))
+}
+
+class CustomMultipleDotSpan : LineBackgroundSpan {
+    private val radius: Float
+    private var color: Int
+
+    constructor() {
+        this.radius = DEFAULT_RADIUS
+        this.color = Color.parseColor("303f9f")
     }
-    private fun grayDecorate(view: DayViewFacade?) {
-        view?.addSpan(DotSpan(7F, Color.parseColor("#455a64")))
+    constructor(color: Int) {
+        this.radius = DEFAULT_RADIUS
+        this.color = Color.parseColor("303f9f")
+    }
+    constructor(radius: Float) {
+        this.radius = radius
+        this.color = Color.parseColor("303f9f")
+    }
+    constructor(radius: Float, color: Int) {
+        this.radius = radius
+        this.color = color
+    }
+
+    override fun drawBackground(canvas: Canvas, paint: Paint, left: Int, right: Int,
+                                top: Int, baseline: Int, bottom: Int, text: CharSequence,
+                                start: Int, end: Int, lineNumber: Int) {
+        val total = if (scheduleList.size > 2) 3 else scheduleList.size
+        var leftMost = (total - 1) * -12
+
+        for (i in 0 until total) {
+            val oldColor = paint.color
+            canvas.drawCircle(((left + right) / 2 - leftMost).toFloat(), bottom + radius, radius, paint)
+            paint.color = oldColor
+            leftMost += 24
+        }
     }
 }
