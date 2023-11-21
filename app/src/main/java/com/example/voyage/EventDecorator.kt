b6540a9bbd91a1cd54696c6a6e94861ba6011768
han1022 <shifty393@gmail.com>
2023-11-21 18:06:01 +0900

    dot 3개 띄우기 성공 수정필요

--- app/src/main/java/com/example/voyage/EventDecorator.kt
+++ app/src/main/java/com/example/voyage/EventDecorator.kt
@@ -13,55 +13,51 @@ import com.prolificinteractive.materialcalendarview.spans.DotSpan.DEFAULT_RADIUS
 
 class EventDecorator(dates: Collection<CalendarDay>) : DayViewDecorator {
     var dates: HashSet<CalendarDay> = HashSet(dates)
+    private lateinit var colors : IntArray
+    private var colorList = ArrayList<Int>()
 
     override fun shouldDecorate(day: CalendarDay?): Boolean {
         return dates.contains(day)
     }
 
     override fun decorate(view: DayViewFacade?) {
-        view?.addSpan(CustomMultipleDotSpan(7f, Color.parseColor("303f9f")))
-    }
-}
+        colorList.add(R.color.red)
+        colorList.add(R.color.orange)
+        colorList.add(R.color.yellow_300)
+        colorList.add(R.color.green_200)
+        colorList.add(R.color.green_700)
+        colorList.add(R.color.blue_200)
+        colorList.add(R.color.blue_700)
+        colorList.add(R.color.brown)
+        colorList.add(R.color.gray)
+        colors = IntArray(colorList.size)
 
-class RedDecorate(dates: Collection<CalendarDay>) : DayViewDecorator {
-    var dates: HashSet<CalendarDay> = HashSet(dates)
-    override fun shouldDecorate(day: CalendarDay?): Boolean {
-        return dates.contains(day)
-    }
-    override fun decorate(view: DayViewFacade?) {
-        view?.addSpan(DotSpan(7F, Color.parseColor("#d32f2f")))
-        Log.d("DecoDates", "$dates")
-    }
-}
-
-class DeepBlueDecorate(dates: Collection<CalendarDay>) : DayViewDecorator {
-    var dates: HashSet<CalendarDay> = HashSet(dates)
-    override fun shouldDecorate(day: CalendarDay?): Boolean {
-        return dates.contains(day)
-    }
-
-    override fun decorate(view: DayViewFacade?) {
-        view?.addSpan(DotSpan(7F, Color.parseColor("#303f9f")))
+        for (i in colorList.indices) {
+            colors[i] = colorList[i]
+        }
+        view?.addSpan(CustomMultipleDotSpan(7f, colors))
+        Log.d("color", "$colorList")
+//        view?.addSpan(CustomMultipleDotSpan(7f, Color.parseColor("303f9f")))
     }
 }
 
 class CustomMultipleDotSpan : LineBackgroundSpan {
     private val radius: Float
-    private var color: Int
+    private var color = IntArray(0)
 
     constructor() {
         this.radius = DEFAULT_RADIUS
-        this.color = Color.parseColor("303f9f")
+        this.color[0] = 0
     }
     constructor(color: Int) {
         this.radius = DEFAULT_RADIUS
-        this.color = Color.parseColor("303f9f")
+        this.color[0] = 0
     }
     constructor(radius: Float) {
         this.radius = radius
-        this.color = Color.parseColor("303f9f")
+        this.color[0] = 0
     }
-    constructor(radius: Float, color: Int) {
+    constructor(radius: Float, color: IntArray) {
         this.radius = radius
         this.color = color
     }
@@ -69,7 +65,7 @@ class CustomMultipleDotSpan : LineBackgroundSpan {
     override fun drawBackground(canvas: Canvas, paint: Paint, left: Int, right: Int,
                                 top: Int, baseline: Int, bottom: Int, text: CharSequence,
                                 start: Int, end: Int, lineNumber: Int) {
-        val total = if (scheduleList.size > 2) 3 else scheduleList.size
+        val total = if (color.size > 2) 3 else color.size
         var leftMost = (total - 1) * -12
 
         for (i in 0 until total) {
